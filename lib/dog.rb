d85c34d32c385a54428c69faa001d811ad1debf3
Heather Kirk <hskirk@gmail.com>
2019-01-31 02:39:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,12 @@ class Dog
   def initialize(name)
     @name = name 
   end 
+   def name=(name)
+     @name = name 
+     
+     def name 
+       @name
+     end 
   
   def breed(mutt)
     @breed = mutt
